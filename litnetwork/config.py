5f708c7df08e218dce11764cdd535e5b8c5e9dcd
Andrew Fitzgerald <fitzgerald.182@osu.edu>
2013-08-30 22:07:39 -0400

    add matrix file config listings

--- litnetwork/config.py
+++ litnetwork/config.py
@@ -8,3 +8,6 @@ processed_data_dir = os.path.join(data_root, 'processed_sources')
 compiled_data_file = os.path.join(data_root, 'compiled_processed_sources.csv')
 reduced_data_file = os.path.join(data_root, 'reduced_network.csv')
 target_hugo_file = os.path.join(data_root, 'target_hugo.txt')
+length_matrix_file = os.path.join(data_root, 'compiled_network_lengths_adj.tab')
+score_matrix_file = os.path.join(data_root, 'compiled_network_score_adj.tab')
+
