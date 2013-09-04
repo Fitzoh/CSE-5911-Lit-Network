631888c4dc2bed39b4eca344c4163d8987f6b13d
Andrew Fitzgerald <fitzgerald.182@osu.edu>
2013-09-04 13:08:09 -0400

    add method to process all steps

--- litnetwork/__init__.py
+++ litnetwork/__init__.py
@@ -1,7 +1,15 @@
 from litnetwork import config as cfg
 import os
+import litnetwork.cleaners as cln
+from litnetwork import compile_processed as cp
+from litnetwork import reduce_network as rn
+from litnetwork import write_matrices as wm
 
 if not os.path.exists(cfg.processed_data_dir):
     os.makedirs(cfg.processed_data_dir)
 
-
+def gen_all():
+    cln.clean_all()
+    cp.compile_all()
+    rn.reduce_network()
+    wm.make_matrices()
