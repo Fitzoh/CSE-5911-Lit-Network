508f55c66c41f9c3ec838dd4f8e0bc0cc1011474
Andrew Fitzgerald <fitzgerald.182@osu.edu>
2013-09-05 23:36:56 -0400

    change input hugo id file

--- litnetwork/config.py
+++ litnetwork/config.py
@@ -7,7 +7,6 @@ unprocessed_data_dir = os.path.join(data_root, 'unprocessed_sources')
 processed_data_dir = os.path.join(data_root, 'processed_sources')
 compiled_data_file = os.path.join(data_root, 'compiled_processed_sources.tab')
 reduced_data_file = os.path.join(data_root, 'reduced_network.tab')
-target_hugo_file = os.path.join(data_root, 'target_hugo.txt')
+target_hugo_file = os.path.join(data_root, 'proteins_per_cell_line.csv')
 length_matrix_file = os.path.join(data_root, 'compiled_network_lengths_adj.tab')
 score_matrix_file = os.path.join(data_root, 'compiled_network_score_adj.tab')
-
